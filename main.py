18430109041fa04654e86356dbf633f1bd2ed1e6
TheHamkerCat <thehamkercat@gmail.com>
2021-05-15 03:57:35 +0530

    Api key

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 from __future__ import unicode_literals
-from Python_ARQ import ARQ
-from colorama import Fore
+
 import asyncio
-import wget
-import youtube_dl
 import os
 
-arq = ARQ("https://thearq.tech")
+import wget
+import youtube_dl
+from colorama import Fore
+from Python_ARQ import ARQ
+
+from config import ARQ_API_KEY
+
+arq = ARQ("https://thearq.tech", ARQ_API_KEY)
 
 
 async def main():
     try:
         while True:
             try:
-                service = int(input(
-                    Fore.YELLOW + ''' \nWhich Service Do You Want To Use? , Enter 0 To Exit\n\n 1. JioSaavn\n 2. Youtube Audio\n 3. Youtube Video\n\n > '''))
+                service = int(
+                    input(
+                        Fore.YELLOW
+                        + """ \nWhich Service Do You Want To Use? , Enter 0 To Exit\n\n 1. JioSaavn\n 2. Youtube Audio\n 3. Youtube Video\n\n > """
+                    )
+                )
             except ValueError:
-                print(Fore.LIGHTGREEN_EX + '\n Exited!\n')
+                print(Fore.LIGHTGREEN_EX + "\n Exited!\n")
                 exit()
 
             try:
@@ -24,15 +32,13 @@ async def main():
                     exit()
 
                 if service == 1:
-                    query = input(Fore.YELLOW + " \nEnter The Name Of Song You Want To Download.\n\n> ")
+                    query = input(
+                        Fore.YELLOW
+                        + " \nEnter The Name Of Song You Want To Download.\n\n> "
+                    )
                     print(Fore.YELLOW + "\nSearching....\n")
-
-                    try:
-                        r = await arq.saavn(query)
-                    except:
-                        print(Fore.RED + "You're Having Some Network Issues, Try To Ping The Server.\n")
-                        exit()
-
+                    r = await arq.saavn(query)
+                    r = r.result
                     n = 0
                     ii = 0
                     count = len(r)
@@ -44,7 +50,11 @@ async def main():
                         print(f"{Fore.LIGHTGREEN_EX} {n}. {song_name}")
                         n += 1
 
-                    song_index = int(input(Fore.YELLOW + " \nEnter Song Number To Download It.\n\n > "))
+                    song_index = int(
+                        input(
+                            Fore.YELLOW + " \nEnter Song Number To Download It.\n\n > "
+                        )
+                    )
                     s = song_index
                     song_name = f"{r[s]['song']}-{r[s].singers}-{r[s].year}"
                     song_link = r[song_index].media_url
@@ -55,64 +65,90 @@ async def main():
                     os.rename(download, final_song_name)
 
                     print(Fore.LIGHTGREEN_EX + " \n\nDownloaded In Current Directory\n")
-                
 
                 elif service == 2:
-                    query = input(Fore.YELLOW + " \nEnter The Name Of Song You Want To Download\Listen.\n\n> ")
+                    query = input(
+                        Fore.YELLOW
+                        + " \nEnter The Name Of Song You Want To Download\Listen.\n\n> "
+                    )
                     print(Fore.YELLOW + "\nSearching....\n")
-                    
-                    results = await arq.youtube(query) 
 
+                    results = await arq.youtube(query)
+                    results = results.result
                     for i in range(len(results)):
                         print(f"{i}. {results[i].title}")
-     
-                    song_index = int(input(Fore.YELLOW + " \nEnter Song Number.\n\n > "))
-                    query2 = int(input(Fore.YELLOW + " \nWhat Do You Want To Do?\n 1. Download\n 2. Listen\n\n> "))
+
+                    song_index = int(
+                        input(Fore.YELLOW + " \nEnter Song Number.\n\n > ")
+                    )
+                    query2 = int(
+                        input(
+                            Fore.YELLOW
+                            + " \nWhat Do You Want To Do?\n 1. Download\n 2. Listen\n\n> "
+                        )
+                    )
                     yt_link = f"https://youtube.com{results[song_index].url_suffix}"
                     if query2 == 1:
                         ydl_opts = {
-                            'format': 'bestaudio/best',
-                            'postprocessors': [{
-                                'key': 'FFmpegExtractAudio',
-                                'preferredcodec': 'mp3',
-                                'preferredquality': '192',
-                            }],
+                            "format": "bestaudio/best",
+                            "postprocessors": [
+                                {
+                                    "key": "FFmpegExtractAudio",
+                                    "preferredcodec": "mp3",
+                                    "preferredquality": "192",
+                                }
+                            ],
                         }
-                        
+
                         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
                             ydl.download([yt_link])
-                        print(Fore.LIGHTGREEN_EX + " \n\nDownloaded In Current Directory\n")
+                        print(
+                            Fore.LIGHTGREEN_EX
+                            + " \n\nDownloaded In Current Directory\n"
+                        )
                     if query2 == 2:
                         print(Fore.YELLOW + "\nLoading...\n")
                         os.system(f"mpv {yt_link} --no-video")
 
-                    
                 elif service == 3:
-                    query = input(Fore.YELLOW + " \nEnter The Name Of Video You Want To Download/Stream.\n\n> ")
+                    query = input(
+                        Fore.YELLOW
+                        + " \nEnter The Name Of Video You Want To Download/Stream.\n\n> "
+                    )
                     print(Fore.YELLOW + "\nSearching....\n")
-                    
-                    results = await arq.youtube(query)
 
+                    results = await arq.youtube(query)
+                    results = results.result
                     for i in range(len(results)):
                         print(f"{i}. {results[i].title}")
-     
-                    song_index = int(input(Fore.YELLOW + " \nEnter Video Number.\n\n > "))
-                    query2 = int(input(Fore.YELLOW + " \nWhat Do You Want To Do?\n 1. Download\n 2. Stream\n\n> "))
+
+                    song_index = int(
+                        input(Fore.YELLOW + " \nEnter Video Number.\n\n > ")
+                    )
+                    query2 = int(
+                        input(
+                            Fore.YELLOW
+                            + " \nWhat Do You Want To Do?\n 1. Download\n 2. Stream\n\n> "
+                        )
+                    )
                     yt_link = f"https://youtube.com{results[song_index].url_suffix}"
                     if query2 == 1:
                         ydl_opts = {}
                         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
                             ydl.download([yt_link])
-                        print(Fore.LIGHTGREEN_EX + " \n\nDownloaded In Current Directory\n")
+                        print(
+                            Fore.LIGHTGREEN_EX
+                            + " \n\nDownloaded In Current Directory\n"
+                        )
                     if query2 == 2:
                         print(Fore.YELLOW + "\nLoading...\n")
                         os.system(f"mpv {yt_link}")
 
             except ValueError:
-                print(Fore.RED + '\n Wrong Input! Try Again.\n')
+                print(Fore.RED + "\n Wrong Input! Try Again.\n")
 
     except KeyboardInterrupt or ValueError:
-        print(Fore.LIGHTGREEN_EX + '\n\n Exited!\n')
+        print(Fore.LIGHTGREEN_EX + "\n\n Exited!\n")
 
 
 asyncio.run(main())
